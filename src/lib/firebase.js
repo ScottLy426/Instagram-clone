af9f819548651544de514f3d755bd2d9142ef78c
Scottly426 <lyscott123@gmail.com>
2021-03-22 15:41:43 -0700

    changing firebase single quotes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyCNIPcjKOBdhNof_xPPYCmDh4fa1vF4Nfw",
-  authDomain: "instagram-clone-850cd.firebaseapp.com",
-  projectId: "instagram-clone-850cd",
-  storageBucket: "instagram-clone-850cd.appspot.com",
-  messagingSenderId: "466943735861",
-  appId: "1:466943735861:web:70fb10a6972d598c05197c",
-  measurementId: "G-XKSYEKHJBD"
+  apiKey: 'AIzaSyCNIPcjKOBdhNof_xPPYCmDh4fa1vF4Nfw',
+  authDomain: 'instagram-clone-850cd.firebaseapp.com',
+  projectId: 'instagram-clone-850cd',
+  storageBucket: 'instagram-clone-850cd.appspot.com',
+  messagingSenderId: '466943735861',
+  appId: '1:466943735861:web:70fb10a6972d598c05197c',
+  measurementId: 'G-XKSYEKHJBD'
 };
 
 const firebase = Firebase.initializeApp(config);
