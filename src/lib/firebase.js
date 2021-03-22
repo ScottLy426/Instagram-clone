ef4de64dfa1dc1952372bdb41450adecc8f365f6
Scottly426 <lyscott123@gmail.com>
2021-03-22 15:55:30 -0700

    changing

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,6 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
 const config = {
   apiKey: 'AIzaSyCNIPcjKOBdhNof_xPPYCmDh4fa1vF4Nfw',
   authDomain: 'instagram-clone-850cd.firebaseapp.com',
@@ -11,6 +10,7 @@ const config = {
   appId: '1:466943735861:web:70fb10a6972d598c05197c',
   measurementId: 'G-XKSYEKHJBD'
 };
+
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
